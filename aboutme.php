d726078cda11fdd60b92aa61fb5829fad574863f
tyt4real <tyt4real@protonmail.com>
2025-07-07 15:47:12 +0200

    Aboutme page

---
+++ aboutme.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+        name="description"
+        content="lain.rocks, the c00l place on the internet" />
+    <meta name="keywords" content="lain" />
+    <meta name="author" content="tyt4real" />
+
+    <title>lain.rocks | About Me</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous" />
+    <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+    <!-- <link href="./dist/config.css" rel="stylesheet" /> 
+     <link href="./dist/index.css" rel="stylesheet" /> -->
+
+    <link href="https://lain.rocks/config.css" rel="stylesheet" />
+    <link href="https://lain.rocks/index.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col">
+                <section class="mt-2 card">
+                    <div class="card-header">
+                        <h2>Who am I?</h2>
+                    </div>
+                    <div class="card-body">
+                        <p>Hi reader, I hope You're doing great this beautiful day, afternoon, evening or whenever You're reading this. I greatly appreciate Your interest in me</p>
+                        <p>
+                        <ul>
+                            <li>
+                                My handle on most places is <i>tyt4real</i>
+                            </li>
+                            <li>
+                                Fediverse: <a href="https://comp.lain.la/users/tyt4real">tyt4real@comp.lain.la</a>
+                            </li>
+                            <li>
+                                XMPP: <a href="xmpp:tyt4real@lain.rocks">tyt4real@lain.rocks</a>
+                            </li>
+                            <li>
+                                GitHub: <a href="https://github.com/tyt4real">tyt4real</a>
+                            </li>
+                        </ul>
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <section class="mt-2 card">
+                    <div class="card-header">
+                        <h2>
+                            About me
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Since a <i>very</i> young age I loved messing around with computers and hardware in general. When I was about 6 I learnt to code in Java for purpose of writing Minecraft server plugins and mods. Since then I learnt many other languages such as C, PHP, Javascript, Typescript and others, although I suck at graphical design, I try my best. Since I begun using the Wired, I switched handles and nicknames many times but I hope my current identity will stay with me for a <i>long</i> time. I have done tons of small side projects, in all of which I've lost interest. This site however I hope will keep me entertained for a very <i>long</i> time.
+                        </p>
+                        <p>
+                            Let's all love Lain.
+                        </p>
+                    </div>
+                </section>
+            </div>
+            <div class="col">
+                <section class="mt-2 card">
+                    <div class="card-header">
+                        <h2>
+                            Contact me?
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            You'd like to contact me? I feel flattered :3
+                        </p>
+                        <ul>
+                            <li>
+                                <ul>
+
+                                    Email: <a href="mailto:tyt4real@protonmail.com">tyt4real@protonmail.com</a>
+
+                                    <li>
+                                        My public PGP key is here: <a href="https://lain.rocks/special/public.asc" target="_blank">here</a>.
+                                    </li>
+                                    <li>
+                                        Key fingerprint:
+                                        <p class="code">
+                                            1CB2748D8D6DED9F
+                                        </p>
+                                    </li>
+                                </ul>
+
+                            </li>
+                            <li>
+                                Most of the time, I'm reachable on #lainchan @ irc.lainchan.org +6697
+                            </li>
+                            <li>
+                                XMPP: <a href="xmpp:tyt4real@lain.rocks">tyt4real@lain.rocks</a> or <a href="xmpp:tyt4real@xmpp.jp">tyt4real@xmpp.jp</a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
