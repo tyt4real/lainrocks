bb8ba6def6ec25bd1d992dabf53830d0bc4abd6c
tyt4real <tyt4real@protonmail.com>
2025-10-23 23:05:09 +0200

    xmpp now working through tor

--- sites/aboutme.php
+++ sites/aboutme.php
@@ -102,7 +102,6 @@
                                         </p>
                                     </li>
                                 </ul>
-
                             </li>
                             <li>
                                 Most of the time, I'm reachable on #lainchan @ irc.lainchan.org +6697
@@ -124,10 +123,4 @@
             </div>
         </section>
     </div>
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
-</body>
-
 </html>
